a04257011db7550f5fb2d1d50f7217110c398231
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-12-15 20:06:23 -0400

    Initial Commit

---
+++ SwiftUIOpenWebinars/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIOpenWebinars
+//
+//  Created by Gustavo Lizárraga on 15/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIOpenWebinars/SwiftUIOpenWebinarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOpenWebinarsApp.swift
+//  SwiftUIOpenWebinars
+//
+//  Created by Gustavo Lizárraga on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOpenWebinarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
